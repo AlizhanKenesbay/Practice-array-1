36a4491d0e17b043e897c2c6cb5d3fe373525131
Alizhan Kenesbay <alzhnk@gmail.com>
2021-09-19 18:29:51 +0600

    Practice

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слова одной строкой через пробел");
+
+        int count = 0;
+        String input = scanner.nextLine();
+
+        if(input.length() != 0)
+            count++;
+            for (int i = 0; i < input.length(); i++) {
+                if(input.charAt(i) == ' '){
+                    count++;
+                }
+            }
+        System.out.println("Колличество слов в предлжений: " + count);
+    }
+}
+
+/*String[] countOfWords = scanner.nextLine().split(" ");
+        for (int i = 0; i < countOfWords.length; i++) {
+            count += i;
+        }
+        System.out.println("Колличество слов в предлжений: " + count);*/
